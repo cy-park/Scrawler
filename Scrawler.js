55c55af860e9b3d8f79aa9ef11a07ed194654647
Chan Young Park <designbycy@gmail.com>
2016-11-29 11:56:04 -0500

    Initial commit

---
+++ Scrawler.js
@@ -0,0 +1,12 @@
+// Supports modern browsers & IE10+
+
+;(function(){
+
+'use strict';
+
+var Scrawler;
+
+if (typeof define === 'function' && define.amd) define(Scrawler); else if (typeof module === 'object' && module.exports) module.exports = Scrawler;
+	this.Scrawler = Scrawler;
+
+}).call(this);
